d4b07aae829aa627b1c13f34faac36ca045058e5
divya <Divya311031@gmail.com>
2026-02-12 22:28:36 +0530

    final production version - Ecommerce website live on InfinityFree

--- includes/connect.php
+++ includes/connect.php
@@ -1,11 +1,11 @@
 <?php
 
 //create the connection with the server name,username,password,database name
-$con=mysqli_connect('localhost:3308','root', '','mystore');
-
+//$con=mysqli_connect('localhost:3308','root', '','mystore');
+$con=mysqli_connect("sql103.infinityfree.com","if0_41125772","Abhi0405MITTAL","if0_41125772_Ecommerce");
 //now check the condition of connection
 if(!$con){
-    die(mysqli_error($con));
+    die("connection failed :" .mysqli_connect_error());
   
 }
 
